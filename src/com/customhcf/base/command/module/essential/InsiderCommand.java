ef5146e7b4212a1727db6a293d20a082a16f2270
unknown <sergivb01@gmail.com>
2017-09-23 20:24:35 +0200

    Insider rank improvements

--- src/com/customhcf/base/command/module/essential/InsiderCommand.java
+++ src/com/customhcf/base/command/module/essential/InsiderCommand.java
@@ -3,6 +3,7 @@ package com.customhcf.base.command.module.essential;
 import com.customhcf.base.BaseConstants;
 import com.customhcf.base.command.BaseCommand;
 import com.customhcf.util.BukkitUtils;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
@@ -18,20 +19,35 @@ public class InsiderCommand
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (args.length < 2) {
-            sender.sendMessage(ChatColor.RED + "Usage: " + this.getUsage(label));
+            sender.sendMessage(this.getUsage(label));
             return true;
         }
         if(!args[0].equalsIgnoreCase("add") || (!args[0].equalsIgnoreCase("remove"))) {
             sender.sendMessage(ChatColor.RED + "Usage: /insider " + ChatColor.AQUA + "<add/remove> " + ChatColor.RED + "<playerName>");
             return true;
         }
-        Player player = (Player) sender;
+
         Player target = BukkitUtils.playerWithNameOrUUID(args[1]);
         if (target == null) {
             sender.sendMessage(String.format(BaseConstants.PLAYER_WITH_NAME_OR_UUID_NOT_FOUND, args[1]));
             return true;
         }
-        sender.sendMessage("pass");
+
+        switch (args[0]){
+            case "add":
+                Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "pex user " + target.getName() + " group add Insider");
+                sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&eYou have added &a" + target.getName() + " &eas Insider."));
+                break;
+            case "remove":
+                Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "pex user " + target.getName() + " group remove Insider");
+                sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&eYou have been removed &a"+ target.getName() + "&e's Insider Rank."));
+                break;
+            default:
+                sender.sendMessage(this.getUsage(label));
+                break;
+        }
+
+
         return true;
     }
 }
