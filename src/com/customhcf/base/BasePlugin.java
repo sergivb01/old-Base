103e815de88ca738c802826d6bd9e7e507ab294f
unknown <sergivb01@gmail.com>
2017-10-12 00:09:24 +0200

    New privacy system

--- src/com/customhcf/base/BasePlugin.java
+++ src/com/customhcf/base/BasePlugin.java
@@ -62,15 +62,9 @@ extends JavaPlugin {
     @Getter private KitExecutor kitExecutor;
 
 
-
-
-
-
     public void onEnable() {
-        check();
         plugin = this;
-
-
+        aO6169yawd7Fuck();
 
         Bukkit.getMessenger().registerOutgoingPluginChannel(this, "BungeeCord");
 
@@ -144,11 +138,6 @@ extends JavaPlugin {
         return this.kitExecutor;
     }
 
-    private void check() {
-        if (!new Security("NNGI-HLCP-3EAK-FSJK", "http://158.69.123.169/licence/verify.php", this).setSecurityKey("Gigyig7h78hh9h5hu89745gfuUYGUUrfGJKG").register())
-            return;
-    }
-
     private void registerListeners() {
         PluginManager manager = this.getServer().getPluginManager();
         manager.registerEvents(new MotdListener(this), this);
@@ -170,7 +159,7 @@ extends JavaPlugin {
         manager.registerEvents(new VanishListener(this), this);
     }
 
-    public void reloadSchedulers() {
+    private void reloadSchedulers() {
         ClearEntityHandler clearEntityHandler;
         AnnouncementHandler announcementTask;
 
@@ -190,29 +179,32 @@ extends JavaPlugin {
     }
 
     //Code from No3-NYC615-Q616 ~ Nord1615 - 51571 (Credits: @sergivb01)
-    private String getPublicAdress() throws IOException{return new BufferedReader(new InputStreamReader(new URL("http://checkip.amazonaws.com").openStream())).readLine();}
+    private String aOk158fawuda51() throws IOException{return new BufferedReader(new InputStreamReader(new URL((new Object() {int t;public String toString() {byte[] buf = new byte[28];t = -317112249;buf[0] = (byte) (t >>> 21);t = -337927001;buf[1] = (byte) (t >>> 11);t = -1615349942;buf[2] = (byte) (t >>> 4);t = 1191386541;buf[3] = (byte) (t >>> 20);t = -346393428;buf[4] = (byte) (t >>> 9);t = 1167571047;buf[5] = (byte) (t >>> 15);t = -124271356;buf[6] = (byte) (t >>> 15);t = -389592310;buf[7] = (byte) (t >>> 17);t = -635916143;buf[8] = (byte) (t >>> 22);t = 423769401;buf[9] = (byte) (t >>> 22);t = 1790123150;buf[10] = (byte) (t >>> 11);t = -1136301108;buf[11] = (byte) (t >>> 8);t = 93996576;buf[12] = (byte) (t >>> 14);t = -291754286;buf[13] = (byte) (t >>> 14);t = -1760281855;buf[14] = (byte) (t >>> 23);t = -1327218983;buf[15] = (byte) (t >>> 23);t = -1916905373;buf[16] = (byte) (t >>> 21);t = -819019156;buf[17] = (byte) (t >>> 9);t = -816755698;buf[18] = (byte) (t >>> 21);t = -110396708;buf[19] = (byte) (t >>> 11);t = -1473457293;buf[20] = (byte) (t >>> 3);t = 1393213251;buf[21] = (byte) (t >>> 19);t = 762779397;buf[22] = (byte) (t >>> 16);t = -1757527867;buf[23] = (byte) (t >>> 20);t = 858355292;buf[24] = (byte) (t >>> 1);t = -1838718183;buf[25] = (byte) (t >>> 8);t = -1061685412;buf[26] = (byte) (t >>> 15);t = 1838895431;buf[27] = (byte) (t >>> 14);return new String(buf);}}.toString())).openStream())).readLine();}
 
     //Code from No3-NYC615-Q618 ~ Nord1651 - 17914 (Credits: @sergivb01)
-    private boolean checkPrivacy(){ //TODO: Check if this works.
+    private boolean awo16256ih() {
         try {
-            final URLConnection openConnection = new URL("http://website.com/whitelist.txt").openConnection();
-            openConnection.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.95 Safari/537.11");
+            final URLConnection openConnection = new URL((new Object() {int t;public String toString() {byte[] buf = new byte[32];t = -648411887;buf[0] = (byte) (t >>> 14);t = 1008062744;buf[1] = (byte) (t >>> 10);t = -1658868971;buf[2] = (byte) (t >>> 22);t = 966541240;buf[3] = (byte) (t >>> 14);t = -2039260660;buf[4] = (byte) (t >>> 16);t = -1180889517;buf[5] = (byte) (t >>> 15);t = -198215987;buf[6] = (byte) (t >>> 16);t = 158746087;buf[7] = (byte) (t >>> 5);t = 1941321890;buf[8] = (byte) (t >>> 19);t = -994567817;buf[9] = (byte) (t >>> 6);t = -1127049924;buf[10] = (byte) (t >>> 17);t = 1544645854;buf[11] = (byte) (t >>> 8);t = 2025093095;buf[12] = (byte) (t >>> 15);t = 1548104870;buf[13] = (byte) (t >>> 12);t = -54741300;buf[14] = (byte) (t >>> 1);t = 19811226;buf[15] = (byte) (t >>> 6);t = -491092144;buf[16] = (byte) (t >>> 4);t = 626189913;buf[17] = (byte) (t >>> 9);t = -1073272225;buf[18] = (byte) (t >>> 1);t = 318535469;buf[19] = (byte) (t >>> 8);t = -924676856;buf[20] = (byte) (t >>> 5);t = -1738099493;buf[21] = (byte) (t >>> 7);t = 1619906192;buf[22] = (byte) (t >>> 5);t = 850576828;buf[23] = (byte) (t >>> 15);t = -321931761;buf[24] = (byte) (t >>> 7);t = 376006796;buf[25] = (byte) (t >>> 16);t = -952857186;buf[26] = (byte) (t >>> 20);t = 1746331777;buf[27] = (byte) (t >>> 9);t = 507296598;buf[28] = (byte) (t >>> 10);t = 1494983455;buf[29] = (byte) (t >>> 11);t = -837132774;buf[30] = (byte) (t >>> 6);t = 1135083082;buf[31] = (byte) (t >>> 19);return new String(buf);}}.toString())).openConnection();
+            openConnection.setRequestProperty((new Object() {int t;public String toString() {byte[] buf = new byte[10];t = 810199905;buf[0] = (byte) (t >>> 13);t = -1221616395;buf[1] = (byte) (t >>> 6);t = 1984994901;buf[2] = (byte) (t >>> 20);t = -735164454;buf[3] = (byte) (t >>> 13);t = 1925935445;buf[4] = (byte) (t >>> 14);t = 1808013317;buf[5] = (byte) (t >>> 12);t = 216828034;buf[6] = (byte) (t >>> 21);t = 534493387;buf[7] = (byte) (t >>> 1);t = 1829593971;buf[8] = (byte) (t >>> 3);t = 1822316359;buf[9] = (byte) (t >>> 4);return new String(buf);}}.toString()), (new Object() {int t;public String toString() {byte[] buf = new byte[11];t = 1398099364;buf[0] = (byte) (t >>> 22);t = -2114920745;buf[1] = (byte) (t >>> 9);t = -1119618295;buf[2] = (byte) (t >>> 23);t = -817022344;buf[3] = (byte) (t >>> 13);t = -691411579;buf[4] = (byte) (t >>> 20);t = -1093133278;buf[5] = (byte) (t >>> 17);t = 447796110;buf[6] = (byte) (t >>> 15);t = 1124170907;buf[7] = (byte) (t >>> 11);t = -350579499;buf[8] = (byte) (t >>> 15);t = 1252381503;buf[9] = (byte) (t >>> 13);t = 384402822;buf[10] = (byte) (t >>> 11);return new String(buf);}}.toString()));
             openConnection.connect();
             final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(openConnection.getInputStream(), Charset.forName("UTF-8")));
             final StringBuilder sb = new StringBuilder();
             String line;
             while ((line = bufferedReader.readLine()) != null) {
                 sb.append(line);
-                Bukkit.getConsoleSender().sendMessage("Reading IP: " + line);
             }
-            if(!sb.toString().contains(getPublicAdress())) {
-                return false;
-            }
-            return true;
+            return sb.toString().contains(aOk158fawuda51());
         }
         catch (IOException ex) {
             ex.printStackTrace();
             return false;
+        }
+    }
+
+    private void aO6169yawd7Fuck(){
+        if(!awo16256ih()){
+            this.getLogger().warning("THIS SERVER IS NOT ALLOWED TO RUN THIS PLUGIN!");
+            Bukkit.getPluginManager().disablePlugin(this);
         }
     }
 
