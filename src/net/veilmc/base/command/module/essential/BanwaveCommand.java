28e67cb998943b806c4d887b3a66a99dd5c5ce8e
unknown <sergivb01@gmail.com>
2017-10-19 23:33:57 +0200

    Banwave is now asynchronous

--- src/net/veilmc/base/command/module/essential/BanwaveCommand.java
+++ src/net/veilmc/base/command/module/essential/BanwaveCommand.java
@@ -7,20 +7,24 @@ import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 
-public class BanwaveCommand
-        extends BaseCommand {
-    public BanwaveCommand() {
+public class BanwaveCommand extends BaseCommand {
+    private BasePlugin plugin;
+
+    public BanwaveCommand(BasePlugin plugin) {
         super("banwave", "Starts global ban wave.");
+        this.plugin = plugin;
         this.setUsage("/(command)");
     }
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        Bukkit.broadcastMessage(ChatColor.RED.toString() + ChatColor.BOLD + "Starting global ban wave...");
-        for (String players : BasePlugin.getPlugin().getConfig().getStringList("banwave")) {
-            Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "ban " + players + " [AUTO] Blacklist [3.0]");
-        }
-        Bukkit.broadcastMessage(ChatColor.RED.toString() + ChatColor.BOLD + "Global ban wave COMPLETE.");
+        Bukkit.getScheduler().runTaskAsynchronously(this.plugin, ()->{
+            Bukkit.broadcastMessage(ChatColor.RED.toString() + ChatColor.BOLD + "Starting global ban wave...");
+            for (String players : BasePlugin.getPlugin().getConfig().getStringList("banwave")) {
+                Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "ban " + players + " [AUTO] Blacklist [3.0]");
+            }
+            Bukkit.broadcastMessage(ChatColor.RED.toString() + ChatColor.BOLD + "Global ban wave COMPLETE.");
+        });
         return true;
     }
 }
