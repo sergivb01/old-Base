aeb1e04804e65111853c888291d91d907687653c
Sergi Vos <sergivb01@gmail.com>
2017-10-26 16:45:38 +0200

    Fix clearlag delay?

--- src/net/veilmc/base/task/ClearEntityHandler.java
+++ src/net/veilmc/base/task/ClearEntityHandler.java
@@ -1,6 +1,7 @@
 
 package net.veilmc.base.task;
 
+import net.veilmc.base.BasePlugin;
 import org.bukkit.Bukkit;
 import org.bukkit.Chunk;
 import org.bukkit.Material;
@@ -14,13 +15,16 @@ import org.bukkit.scheduler.BukkitRunnable;
 public class ClearEntityHandler extends BukkitRunnable {
     public void run() {
         for (World world : Bukkit.getWorlds()) {
+            int i = 0;
             for (Chunk chunk : world.getLoadedChunks()) {
                 for (Entity entity : chunk.getEntities()) {
                     Material material;
                     if (entity.getType() != EntityType.DROPPED_ITEM && entity.getType() != EntityType.SKELETON && entity.getType() != EntityType.ZOMBIE && entity.getType() != EntityType.ARROW && entity.getType() != EntityType.SPIDER && entity.getType() != EntityType.CREEPER && entity.getType() != EntityType.COW && entity.getType() != EntityType.ENDERMAN && entity.getType() != EntityType.SILVERFISH && entity.getType() != EntityType.MAGMA_CUBE && entity.getType() != EntityType.IRON_GOLEM && entity.getType() != EntityType.GHAST || entity instanceof Tameable && ((Tameable)entity).isTamed() || entity.getType() == EntityType.DROPPED_ITEM && entity instanceof Item && ((material = ((Item) entity).getItemStack().getType()) == Material.SAND || material == Material.TNT || material == Material.DIAMOND || material == Material.EMERALD || material == Material.DIAMOND_ORE || material == Material.GOLD_ORE || material == Material.GOLD_INGOT || material == Material.IRON_INGOT || material == Material.IRON_ORE || material == Material.MOB_SPAWNER || material == Material.HOPPER || material == Material.BEACON)) continue;
                     entity.remove();
+                    i++;
                 }
             }
+            BasePlugin.getPlugin().getLogger().info("Cleared " + i + " entities in " + world.getName() + "!");
         }
     }
 
