5a6a85b2201fb253bcd5c6c3dd3e406cbab62ccd
explance <lentsalts@gmail.com>
2017-11-27 18:02:26 +0000

    i'm noob

--- src/net/veilmc/base/command/module/essential/SeeCommand.java
+++ src/net/veilmc/base/command/module/essential/SeeCommand.java
@@ -5,13 +5,19 @@ import net.veilmc.base.BaseConstants;
 import net.veilmc.base.BasePlugin;
 import net.veilmc.base.command.BaseCommand;
 import net.veilmc.util.BukkitUtils;
+import net.veilmc.util.menu.Menu;
+import net.veilmc.util.menu.mask.Mask;
+import net.veilmc.util.menu.mask.Mask2D;
+import net.veilmc.util.menu.type.ChestMenu;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Material;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
 
 import java.io.IOException;
 import java.sql.PreparedStatement;
@@ -20,15 +26,37 @@ import java.sql.SQLException;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
+import static java.awt.SystemColor.menu;
+
 public class SeeCommand extends BaseCommand {
 
     private final BasePlugin plugin;
-    public Inventory page1 = Bukkit.createInventory(null, 36, "Punishment Manager");
-
     public SeeCommand(BasePlugin plugin) {
         super("see", "Blah");
         this.setUsage("/(command) <player>");
         this.plugin = plugin;
+    }
+
+
+    public Menu createMenu() {
+        return ChestMenu.builder(4)
+                .title(ChatColor.translateAlternateColorCodes('&', "&bPunishment GUI"))
+                .build();
+    }
+    public void displayMenu(Player player) {
+        Menu menu = createMenu();
+        menu.open(player);
+    }
+    public void addWhiteBorder(Menu menu) {
+        ItemStack glass = new ItemStack(Material.STAINED_GLASS_PANE);
+        for (int i = 0 ; i < 9 ; i++) {
+            menu.getSlot(i).setItem(glass);
+        }
+        menu.getSlot(17).setItem(glass);
+        menu.getSlot(18).setItem(glass);
+        for (int i = 26 ; i < 36 ; i++) {
+            menu.getSlot(i).setItem(glass);
+        }
     }
 
     @Override
@@ -47,35 +75,34 @@ public class SeeCommand extends BaseCommand {
 
         UUID uuid = target.getUniqueId();
 
-        new Thread(()->{
-            Bukkit.getScheduler().runTaskAsynchronously(this.plugin, ()->{
-
-
-                boolean banned = Database.get().isPlayerBanned(uuid, "null");
-                boolean muted = Database.get().isPlayerMuted(uuid, "null");
-                p.sendMessage("start");
-                if(banned) {
-                    p.sendMessage("banned");
-                } else {
-                    p.sendMessage("NOT banned");
-                }
-                if(muted) {
-                    p.sendMessage("muted");
-                } else {
-                    p.sendMessage("NOT muted");
-                }
-                p.sendMessage("end");
-                try (PreparedStatement st = Database.get().prepareStatement("SELECT * FROM {bans}")) {
-                    try (ResultSet rs = st.executeQuery()) {
-        /* ... */
-                    }
-
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-
-            });
-        }).start();
+        createMenu();
+        addWhiteBorder(createMenu());
+        displayMenu(p);
+        p.sendMessage("OPEN");
+//        new Thread(()->{
+//            Bukkit.getScheduler().runTaskAsynchronously(this.plugin, ()->{
+//
+//
+//                boolean banned = Database.get().isPlayerBanned(uuid, "null");
+//                boolean muted = Database.get().isPlayerMuted(uuid, "null");
+//                p.sendMessage("start");
+//                if(banned) {
+//                    p.sendMessage("banned");
+//                } else {
+//                    p.sendMessage("NOT banned");
+//                }
+//                if(muted) {
+//                    p.sendMessage("muted");
+//                } else {
+//                    p.sendMessage("NOT muted");
+//                }
+//                p.sendMessage("end");
+//               // createMenu();
+//                addWhiteBorder(createMenu());
+//                displayMenu(p);
+//
+//            });
+//        }).start();
 
         return true;
     }
