39a851216a5d61afc5e31e60f1136044782c1651
Sergi <sergivb01@gmaill.com>
2018-01-30 20:52:44 +0100

    Simplified a lot of code on PlayerChatEvent (mutechat lag fixed?)

--- src/net/veilmc/base/listener/ChatListener.java
+++ src/net/veilmc/base/listener/ChatListener.java
@@ -36,7 +36,7 @@ public class ChatListener
         UUID uuid = player.getUniqueId();
         String name = player.getName();
         BaseUser baseUser = this.plugin.getUserManager().getUser(uuid);
-        Iterator iterator = event.getRecipients().iterator();
+        /*Iterator iterator = event.getRecipients().iterator(); //Simplified down theree
         while (iterator.hasNext()) {
             Player target = (Player)iterator.next();
             BaseUser targetUser = this.plugin.getUserManager().getUser(target.getUniqueId());
@@ -50,40 +50,21 @@ public class ChatListener
             }
             if (targetUser.isGlobalChatVisible()) continue;
             iterator.remove();
+        }*/
+
+        for(Player target : event.getRecipients()){
+            BaseUser targetUser = this.plugin.getUserManager().getUser(target.getUniqueId());
+            if((baseUser.isInStaffChat() && !targetUser.isStaffChatVisible()) || targetUser.getIgnoring().contains(name) || targetUser.isGlobalChatVisible()){
+                event.getRecipients().remove(target);
+            }
         }
-//        if (baseUser.isInStaffChat()) {
-//            final Set<CommandSender> staffChattable = Sets.newHashSet();
-//            for (final Permissible permissible : Bukkit.getServer().getOnlinePlayers()) {
-//                if (permissible.hasPermission("command.staffchat") && permissible instanceof CommandSender) {
-//                    staffChattable.add((CommandSender) permissible);
-//                }
-//            }
-//            if (staffChattable.contains(player) && baseUser.isInStaffChat()) {
-//                final String format = ChatColor.AQUA + String.format(Locale.ENGLISH, ChatColor.BLUE + "(Staff) " + ChatColor.AQUA + "%1$s" + ChatColor.GRAY + ": " + ChatColor.AQUA + "%2$s", player.getName(), event.getMessage());
-//                for (final CommandSender target2 : staffChattable) {
-//                    if (target2 instanceof Player) {
-//                        final Player targetPlayer = (Player)target2;
-//                        final BaseUser targetUser2 = this.plugin.getUserManager().getUser(targetPlayer.getUniqueId());
-//                        if (targetUser2.isStaffChatVisible()) {
-//                            target2.sendMessage(format);
-//                        }
-//                        else {
-//                            if (!target2.equals(player)) {
-//                                continue;
-//                            }
-//                            target2.sendMessage(ChatColor.RED + "Your message was sent, but you cannot see staff chat messages as your notifications are disabled: Use /togglesc.");
-//                        }
-//                    }
-//                }
-//                event.setCancelled(true);
-//                return;
-//            }
-//        }
+
         if ((remainingChatDisabled = this.plugin.getServerHandler().getRemainingChatDisabledMillis()) > 0 && !player.hasPermission("rank.staff")) {
-            event.setCancelled(true);
             player.sendMessage(ChatColor.RED + "Global chat is currently disabled for another " + ChatColor.RED + DurationFormatUtils.formatDurationWords(remainingChatDisabled, true, true) + ChatColor.RED + '.');
+            event.setCancelled(true);
             return;
         }
+
         long remainingChatSlowed = this.plugin.getServerHandler().getRemainingChatSlowedMillis();
         if (remainingChatSlowed > 0 && !player.hasPermission("slowchat.bypass")) {
             long speakTimeRemaining = baseUser.getLastSpeakTimeRemaining();
@@ -140,5 +121,42 @@ public class ChatListener
         }
     }
 
+
+    /*
+    From onPlayerChat
+
+
+
+//        if (baseUser.isInStaffChat()) {
+//            final Set<CommandSender> staffChattable = Sets.newHashSet();
+//            for (final Permissible permissible : Bukkit.getServer().getOnlinePlayers()) {
+//                if (permissible.hasPermission("command.staffchat") && permissible instanceof CommandSender) {
+//                    staffChattable.add((CommandSender) permissible);
+//                }
+//            }
+//            if (staffChattable.contains(player) && baseUser.isInStaffChat()) {
+//                final String format = ChatColor.AQUA + String.format(Locale.ENGLISH, ChatColor.BLUE + "(Staff) " + ChatColor.AQUA + "%1$s" + ChatColor.GRAY + ": " + ChatColor.AQUA + "%2$s", player.getName(), event.getMessage());
+//                for (final CommandSender target2 : staffChattable) {
+//                    if (target2 instanceof Player) {
+//                        final Player targetPlayer = (Player)target2;
+//                        final BaseUser targetUser2 = this.plugin.getUserManager().getUser(targetPlayer.getUniqueId());
+//                        if (targetUser2.isStaffChatVisible()) {
+//                            target2.sendMessage(format);
+//                        }
+//                        else {
+//                            if (!target2.equals(player)) {
+//                                continue;
+//                            }
+//                            target2.sendMessage(ChatColor.RED + "Your message was sent, but you cannot see staff chat messages as your notifications are disabled: Use /togglesc.");
+//                        }
+//                    }
+//                }
+//                event.setCancelled(true);
+//                return;
+//            }
+//        }
+
+
+     */
 
 }
