bb46aa64aadeb2de11a296fa58a87d37c331ef97
iDaniel84 <idaniel84yt@gmai.com>
2018-02-20 13:00:32 +0100

    Insider command fixed

--- src/net/veilmc/base/command/module/essential/InsiderCommand.java
+++ src/net/veilmc/base/command/module/essential/InsiderCommand.java
@@ -4,7 +4,6 @@ import net.veilmc.base.BaseConstants;
 import net.veilmc.base.BasePlugin;
 import net.veilmc.base.command.BaseCommand;
 import net.veilmc.base.user.BaseUser;
-import net.veilmc.util.API;
 import net.veilmc.util.BukkitUtils;
 import org.apache.commons.lang.time.DateFormatUtils;
 import org.bukkit.Bukkit;
@@ -12,6 +11,9 @@ import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+
+import java.util.Collections;
+import java.util.List;
 
 public class InsiderCommand extends BaseCommand {
     private BasePlugin plugin;
@@ -24,28 +26,28 @@ public class InsiderCommand extends BaseCommand {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if (args.length < 2) {
-            sender.sendMessage(this.getUsage(label));
-            return true;
-        }
-        if (!(args[0].equalsIgnoreCase("add")) || (!(args[0].equalsIgnoreCase("remove")))) {
-            sender.sendMessage("ARGS0: " + args[0]);
-            sender.sendMessage("ARGS1: " + args[1]);
-            sender.sendMessage(ChatColor.RED + "Usage: /insider " + ChatColor.AQUA + "<add/remove> " + ChatColor.RED + "<playerName>");
+        if (!(args.length == 2)) {
+            sender.sendMessage(this.getUsage());
             return true;
         }
 
+        /* THIS THING CALL 2 METHODS. IF IT IS ADD IT WONT BE REMOVE, SAME IF IT IS REMOVE IT WONT BE ADD
+        if (!args[0].equalsIgnoreCase("add") || !args[0].equalsIgnoreCase("remove")) {
+            sender.sendMessage("ARGS0: " + args[0]);
+            sender.sendMessage("ARGS1: " + args[1]);
+            sender.sendMessage(ChatColor.RED + "Usage: /insider " + ChatColor.AQUA + "<add/remove> " + ChatColor.RED + "<playerName>");
+           return true;
+        }*/
+
         Player target = BukkitUtils.playerWithNameOrUUID(args[1]);
-        if (target == null) {
+        if (((args[0].equalsIgnoreCase("add") || args[0].equalsIgnoreCase("remove")) && target == null) || target == null) {
             sender.sendMessage(String.format(BaseConstants.PLAYER_WITH_NAME_OR_UUID_NOT_FOUND, args[1]));
             return true;
         }
 
-
         BaseUser baseUser = this.plugin.getUserManager().getUser(target.getUniqueId());
         String upTime = DateFormatUtils.format(System.currentTimeMillis(), "dd/MM");
         String time = DateFormatUtils.format(System.currentTimeMillis(), "hh:mm");
-
         if (args[0].equalsIgnoreCase("add")) {
             Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "pex user " + target.getName() + " group add Insider");
             sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&eYou have added &a" + target.getName() + " &eas Insider."));
@@ -54,11 +56,16 @@ public class InsiderCommand extends BaseCommand {
         } else if (args[0].equalsIgnoreCase("remove")) {
             Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "pex user " + target.getName() + " group remove Insider");
             sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&eYou have been removed &a" + target.getName() + "&e's Insider Rank."));
-            baseUser.setNote(ChatColor.YELLOW + "Staff: " + ChatColor.GREEN + sender.getName() + ChatColor.GRAY + " [" + upTime + "-" + time + "]" + " - " + ChatColor.YELLOW + "Set player as "+ ChatColor.RED + "" + ChatColor.BOLD + "NOT" + ChatColor.YELLOW + " insider.");
+            baseUser.setNote(ChatColor.YELLOW + "Staff: " + ChatColor.GREEN + sender.getName() + ChatColor.GRAY + " [" + upTime + "-" + time + "]" + " - " + ChatColor.YELLOW + "Set player as " + ChatColor.RED + "" + ChatColor.BOLD + "NOT" + ChatColor.YELLOW + " insider.");
             return true;
         } else {
-            sender.sendMessage(this.getUsage(label));
-            return false;
+            sender.sendMessage(this.getUsage());
+            return true;
         }
+    }
+
+    @Override
+    public List<String> onTabComplete(CommandSender sender, Command command, String label, String[] args) {
+        return args.length == 2 ? null : Collections.emptyList();
     }
 }
